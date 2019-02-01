0723f6ba26016ed5f74b2d76af92f02aa904e156
frostmarry <frostmarrys@gmail.com>
2019-02-01 11:18:25 +0700

    sembarang

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,112 +10,112 @@ class Admin extends CI_Controller {
 	// {
  //        switch ($tunjukin) {
  //        	case 'overview':
- //        		$this->load->view('index');
+ //        		$this->load->view('admin/index');
  //        		break;
  //        	case 'absensi':
- //        		$this->load->view('absensi');
+ //        		$this->load->view('admin/absensi');
  //        		break;
  //        	case 'belajar':
- //        		$this->load->view('belajar');
+ //        		$this->load->view('admin/belajar');
  //        		break;
  //        	case 'berita':
- //        		$this->load->view('berita');
+ //        		$this->load->view('admin/berita');
  //        		break;
  //        	case 'imageslider':
- //        		$this->load->view('imageslider');
+ //        		$this->load->view('admin/imageslider');
  //        		break;
  //        	case 'informasi':
- //        		$this->load->view('informasi');
+ //        		$this->load->view('admin/informasi');
  //        		break;
  //        	case 'jadwal':
- //        		$this->load->view('jadwal');
+ //        		$this->load->view('admin/jadwal');
  //        		break;
  //        	case 'nilai':
- //        		$this->load->view('nilai');
+ //        		$this->load->view('admin/nilai');
  //        		break;
  //        	case 'pembayaran':
- //        		$this->load->view('pembayaran');
+ //        		$this->load->view('admin/pembayaran');
  //        		break;
  //        	case 'users':
- //        		$this->load->view('users');
+ //        		$this->load->view('admin/users');
  //        		break;
  //        	case 'admin':
- //        		$this->load->view('admin');
+ //        		$this->load->view('admin/admin');
  //        		break;
  //        	case 'guruabsen':
- //        		$this->load->view('guruabsen');
+ //        		$this->load->view('admin/guruabsen');
  //        		break;
  //        	case 'kelas':
- //        		$this->load->view('kelas');
+ //        		$this->load->view('admin/kelas');
  //        		break;
  //        	default:
- //        		$this->load->view('index');
+ //        		$this->load->view('admin/index');
  //        		break;
  //        }
 	// }
 	public function index()
 	{
         
-       $this->load->view('index');
+       $this->load->view('admin/index');
 	}
 	public function absensi()
 	{
         
-        $this->load->view('absensi');
+        $this->load->view('admin/absensi');
 	}
 	public function admin()
 	{
         
-        $this->load->view('admin');
+        $this->load->view('admin/admin');
 	}
 	public function belajar()
 	{
         
-        $this->load->view('belajar');
+        $this->load->view('admin/belajar');
 	}
 	public function berita()
 	{
         
-        $this->load->view('berita');
+        $this->load->view('admin/berita');
 	}
 	public function guruabsen()
 	{
         
-        $this->load->view('guruabsen');
+        $this->load->view('admin/guruabsen');
 	}
 	public function imageslider()
 	{
         
-        $this->load->view('imageslider');
+        $this->load->view('admin/imageslider');
 	}
 	public function informasi()
 	{
         
-        $this->load->view('informasi');
+        $this->load->view('admin/informasi');
 	}
 	public function jadwal()
 	{
         
-        $this->load->view('jadwal');
+        $this->load->view('admin/jadwal');
 	}
 	public function kelas()
 	{
         
-        $this->load->view('kelas');
+        $this->load->view('admin/kelas');
 	}
 	public function nilai()
 	{
         
-        $this->load->view('nilai');
+        $this->load->view('admin/nilai');
 	}
 	public function pembayaran()
 	{
         
-        $this->load->view('pembayaran');
+        $this->load->view('admin/pembayaran');
 	}
 	public function users()
 	{
         
-        $this->load->view('users');
+        $this->load->view('admin/users');
 	}
 }
---
+++ application/views/admin/_partials/sidebar.php
@@ -0,0 +1,70 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'index' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/index') ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'absensi' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/absensi') ?>">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Absensi</span>
+        </a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'belajar' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/belajar') ?>">
+            <i class="fas fa-fw fa-graduation-cap"></i>
+            <span>Belajar</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'berita' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/berita') ?>">
+            <i class="fas fa-fw fa-newspaper"></i>
+            <span>Berita</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'imageslider' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/imageslider') ?>">
+            <i class="fas fa-fw fa-image"></i>
+            <span>Image Slider</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'informasi' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/informasi') ?>">
+            <i class="fas fa-fw fa-info"></i>
+            <span>Informasi</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'jadwal' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/jadwal') ?>">
+            <i class="fas fa-fw fa-clipboard-list"></i>
+            <span>Jadwal</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'nilai' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/nilai') ?>">
+            <i class="fas fa-fw fa-list-ol"></i>
+            <span>Nilai</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'pembayaran' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/pembayaran') ?>">
+            <i class="fas fa-fw fa-money-bill-alt"></i>
+            <span>Pembayaran</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'users' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/users') ?>">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Users</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'admin' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/admin') ?>">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Admin</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'guruabsen' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/guruabsen') ?>">
+            <i class="fas fa-fw fa-clipboard-list"></i>
+            <span>Guru Absen</span></a>
+    </li>
+    <li class="nav-item <?php echo $this->uri->segment(3) == 'kelas' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('admin/kelas') ?>">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Kelas</span></a>
+    </li>
+</ul>
