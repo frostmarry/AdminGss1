9fb0e7ad2151d6b22074a84e12bc4abf7c889a4c
frostmarry <frostmarrys@gmail.com>
2019-02-01 10:37:27 +0700

    bitch

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,112 +10,112 @@ class Admin extends CI_Controller {
 	// {
  //        switch ($tunjukin) {
  //        	case 'overview':
- //        		$this->load->view('admin/index');
+ //        		$this->load->view('index');
  //        		break;
  //        	case 'absensi':
- //        		$this->load->view('admin/absensi');
+ //        		$this->load->view('absensi');
  //        		break;
  //        	case 'belajar':
- //        		$this->load->view('admin/belajar');
+ //        		$this->load->view('belajar');
  //        		break;
  //        	case 'berita':
- //        		$this->load->view('admin/berita');
+ //        		$this->load->view('berita');
  //        		break;
  //        	case 'imageslider':
- //        		$this->load->view('admin/imageslider');
+ //        		$this->load->view('imageslider');
  //        		break;
  //        	case 'informasi':
- //        		$this->load->view('admin/informasi');
+ //        		$this->load->view('informasi');
  //        		break;
  //        	case 'jadwal':
- //        		$this->load->view('admin/jadwal');
+ //        		$this->load->view('jadwal');
  //        		break;
  //        	case 'nilai':
- //        		$this->load->view('admin/nilai');
+ //        		$this->load->view('nilai');
  //        		break;
  //        	case 'pembayaran':
- //        		$this->load->view('admin/pembayaran');
+ //        		$this->load->view('pembayaran');
  //        		break;
  //        	case 'users':
- //        		$this->load->view('admin/users');
+ //        		$this->load->view('users');
  //        		break;
  //        	case 'admin':
- //        		$this->load->view('admin/admin');
+ //        		$this->load->view('admin');
  //        		break;
  //        	case 'guruabsen':
- //        		$this->load->view('admin/guruabsen');
+ //        		$this->load->view('guruabsen');
  //        		break;
  //        	case 'kelas':
- //        		$this->load->view('admin/kelas');
+ //        		$this->load->view('kelas');
  //        		break;
  //        	default:
- //        		$this->load->view('admin/index');
+ //        		$this->load->view('index');
  //        		break;
  //        }
 	// }
-	public function overview()
+	public function index()
 	{
         
-       $this->load->view('admin/index');
+       $this->load->view('index');
 	}
 	public function absensi()
 	{
         
-        $this->load->view('admin/absensi');
+        $this->load->view('absensi');
 	}
 	public function admin()
 	{
         
-        $this->load->view('admin/admin');
+        $this->load->view('admin');
 	}
 	public function belajar()
 	{
         
-        $this->load->view('admin/belajar');
+        $this->load->view('belajar');
 	}
 	public function berita()
 	{
         
-        $this->load->view('admin/berita');
+        $this->load->view('berita');
 	}
 	public function guruabsen()
 	{
         
-        $this->load->view('admin/guruabsen');
+        $this->load->view('guruabsen');
 	}
 	public function imageslider()
 	{
         
-        $this->load->view('admin/imageslider');
+        $this->load->view('imageslider');
 	}
 	public function informasi()
 	{
         
-        $this->load->view('admin/informasi');
+        $this->load->view('informasi');
 	}
 	public function jadwal()
 	{
         
-        $this->load->view('admin/jadwal');
+        $this->load->view('jadwal');
 	}
 	public function kelas()
 	{
         
-        $this->load->view('admin/kelas');
+        $this->load->view('kelas');
 	}
 	public function nilai()
 	{
         
-        $this->load->view('admin/nilai');
+        $this->load->view('nilai');
 	}
 	public function pembayaran()
 	{
         
-        $this->load->view('admin/pembayaran');
+        $this->load->view('pembayaran');
 	}
 	public function users()
 	{
         
-        $this->load->view('admin/users');
+        $this->load->view('users');
 	}
 }
